126aef591253736a2f69a03853379ff9b868bad9
shilinghuai <slhzxm16@aliyun.com>
2019-03-29 09:30:30 +0800

    book表查询

---
+++ shilinghuai/yu/bookselect/select.js
@@ -0,0 +1,172 @@
+/**
+ * @Created by shilinghuai shilh2@yusys.com.cn on 2019-3-26 10:59:48.
+ * @updated by
+ * @description select
+ */
+define([
+  './custom/widgets/js/YufpDemoSelector.js',
+  'libs/js-xlsx/xlsx.full.min.js'
+], function (require, exports) {
+  /**
+   * 页面加载完成时触发
+   * @param hashCode 路由ID
+   * @param data 传递数据对象
+   * @param cite 页面站点信息
+   */
+  exports.ready = function (hashCode, data, cite) {
+    yufp.lookup.reg('CRUD_TYPE,NATIONALITY,PUBLISH_STATUS');
+    yufp.custom.vue({
+      el: cite.el,
+      data: function () {
+        return {
+          dataUrl: '/trade/example/list',
+          formdata: {},
+          rule: [
+            { required: true, message: '必填项', trigger: 'blur' },
+            { validator: yufp.validator.number, message: '数字', trigger: 'blur' }
+          ],
+          dialogVisible: false,
+          formDisabled: false,
+          viewType: 'DETAIL',
+          viewTitle: yufp.lookup.find('CRUD_TYPE', false),
+          saveBtnShow: true
+        };
+      },
+      methods: {
+        /**
+         * 取消
+         */
+        cancelFn: function () {
+          var _this = this;
+          _this.dialogVisible = false;
+        },
+        /**
+         * 保存
+         */
+        saveFn: function () {
+          var _this = this;
+          var model = {};
+          yufp.clone(_this.formdata, model);
+          var validate = false;
+          _this.$refs.refForm.validate(function (valid) {
+            validate = valid;
+          });
+          if (!validate) {
+            return;
+          }
+          // 向后台发送保存请求
+          yufp.service.request({
+            method: 'POST',
+            url: '/trade/example/save',
+            data: model,
+            callback: function (code, message, response) {
+              _this.$refs.refTable.remoteData();
+              _this.$message('操作成功');
+              _this.dialogVisible = false;
+            }
+          });
+        },
+        /**
+         * 控制保存按钮、xdialog、表单的状态
+        * @param viewType 表单类型
+        * @param editable 可编辑,默认false
+        */
+        switchStatus: function (viewType, editable) {
+          var _this = this;
+          _this.viewType = viewType;
+          _this.saveBtnShow = editable;
+          _this.dialogVisible = true;
+          _this.formDisabled = !editable;
+        },
+        /**
+         * 新增按钮
+         */
+        addFn: function () {
+          var _this = this;
+          _this.switchStatus('ADD', true);
+          _this.$nextTick(function () {
+            _this.$refs.refForm.resetFields();
+          });
+        },
+        /**
+         * 修改
+         */
+        modifyFn: function () {
+          var _this = this;
+          if (_this.$refs.refTable.selections.length != 1) {
+            _this.$message({ message: '请先选择一条记录', type: 'warning' });
+            return;
+          }
+          _this.switchStatus('EDIT', true);
+          _this.$nextTick(function () {
+            _this.$refs.refForm.resetFields();
+            var obj = _this.$refs.refTable.selections[0];
+            yufp.clone(obj, _this.formdata);
+          });
+        },
+        /**
+         * 详情
+         */
+        infoFn: function () {
+          var _this = this;
+          var selectionsAry = _this.$refs.refTable.selections;
+          if (selectionsAry.length != 1) {
+            _this.$message({ message: '请先选择一条记录', type: 'warning' });
+            return;
+          }
+          _this.switchStatus('DETAIL', false);
+          _this.$nextTick(function () {
+            _this.$refs.refForm.resetFields();
+            yufp.clone(selectionsAry[0], _this.formdata);
+          });
+        },
+        /**
+         * 删除
+         */
+        deleteFn: function () {
+          var _this = this;
+          var selections = _this.$refs.refTable.selections;
+          if (selections.length < 1) {
+            _this.$message({ message: '请先选择一条记录', type: 'warning' });
+            return;
+          }
+          var len = selections.length, arr = [];
+          for (var i = 0; i < len; i++) {
+            arr.push(selections[i].id);
+          }
+          _this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+            center: true,
+            callback: function (action) {
+              if (action === 'confirm') {
+                yufp.service.request({
+                  method: 'POST',
+                  url: '/trade/example/delete',
+                  data: {
+                    ids: arr.join(',')
+                  },
+                  callback: function (code, message, response) {
+                    _this.$refs.refTable.remoteData();
+                    _this.$message('操作成功');
+                  }
+                });
+              }
+            }
+          });
+        },
+        /**
+         * 导出操作
+         */
+        exportFn: function () {
+          var _this = this;
+          yufp.util.exportExcelByTable({
+            ref: _this.$refs.refTable,
+            url: '/trade/example/list'
+          });
+        }
+      }
+    });
+  };
+});

fc9d1699c1f5578071f9d48b4d0958035a88503e
shilinghuai <slhzxm16@aliyun.com>
2019-04-01 18:37:05 +0800

    yu基础组件

---
+++ shilinghuai/yu/component/component.js
@@ -0,0 +1,43 @@
+/**
+ * @created by shilinghuai on 2019-4-1 18:07:11
+ * @updated by
+ * @description 组件
+ */
+define(function (require, exports) {
+  /**
+   * 页面加载完成时触发
+   * @param hashCode 路由ID
+   * @param data 传递数据对象
+   * @param cite 页面站点信息
+   */
+  exports.ready = function (hashCode, data, cite) {
+    yufp.custom.vue({
+      el: cite.el,
+      data: function () {
+        return {
+          startTime: '',
+          endTime: ''
+        };
+      },
+      methods: {
+
+      }
+    });
+  };
+
+  /**
+   * 页面传递消息处理
+   * @param type 消息类型
+   * @param message 消息内容
+   */
+  exports.onmessage = function (type, message) {
+  };
+
+  /**
+   * 页面销毁时触发destroy方法
+   * @param id 路由ID
+   * @param cite 页面站点信息
+   */
+  exports.destroy = function (id, cite) {
+  };
+});

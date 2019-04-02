8a71ad2215df00f9a3f50dea7201ae6be7b0c6f9
Yoo Fale <645406586@qq.com>
2019-04-02 09:43:46 +0800

    一些组件

---
+++ yaozuopeng/yu-tabs/yu-tabs.js
@@ -0,0 +1,45 @@
+/**
+ * @created by yaozuopeng on 2019-4-1 18:36:29
+ * @updated by
+ * @description 选项卡
+ */
+define(function (require, exports) {
+  /**
+   * 页面加载完成时触发
+   * @param hashCode 路由ID
+   * @param data 传递数据对象
+   * @param cite 页面站点信息
+   */
+  exports.ready = function (hashCode, data, cite) {
+    yufp.custom.vue({
+      el: cite.el,
+      data: function () {
+        return {
+          tab: ''
+        };
+      },
+      methods: {
+        cl: function () {
+          alert(this.tab);
+        }
+      }
+
+    });
+  };
+
+  /**
+   * 页面传递消息处理
+   * @param type 消息类型
+   * @param message 消息内容
+   */
+  exports.onmessage = function (type, message) {
+  };
+
+  /**
+   * 页面销毁时触发destroy方法
+   * @param id 路由ID
+   * @param cite 页面站点信息
+   */
+  exports.destroy = function (id, cite) {
+  };
+});
